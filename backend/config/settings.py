b7efbc4ae981e9e612bd2df05c9ff73c90001565
Itzel conde <chelconde46@gmail.com>
2026-03-19 22:37:13 -0600

    Autenticacion

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -121,12 +121,10 @@ if NETLIFY_URL:
 CORS_ALLOW_ALL_ORIGINS = True
 
 # ── Email ──────────────────────────────────────────────────
-FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:5173")
-
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
 EMAIL_PORT          = 587
 EMAIL_USE_TLS       = True
-EMAIL_HOST_USER     = 'webbiblioteca09@gmail.com'
-EMAIL_HOST_PASSWORD = 'sbmydzladnxootyp'
-DEFAULT_FROM_EMAIL  = 'Biblioteca <webbiblioteca09@gmail.com>'
+EMAIL_HOST_USER     = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL  = f'Biblioteca <{os.environ.get("EMAIL_HOST_USER", "")}>'
